0dce6aeb2410a870a73c49a5c12847978d00015c
mattcraigie <matthewcraigie1@gmail.com>
2023-06-09 16:57:38 +1000

    steup for interactive node

--- scripts/subset_test_ost_ddp.py
+++ scripts/subset_test_ost_ddp.py
@@ -14,8 +14,9 @@ from ostlensing.models import OptimisableSTRegressor
 # DDP functions
 
 def setup(rank, world_size):
-    # os.environ['MASTER_ADDR'] = 'localhost'
-    # os.environ['MASTER_PORT'] = '12355'
+    # use these if using a notebook
+    os.environ['MASTER_ADDR'] = 'localhost'
+    os.environ['MASTER_PORT'] = '12355'
 
     # initialize the process group
     dist.init_process_group("gloo", rank=rank, world_size=world_size)
