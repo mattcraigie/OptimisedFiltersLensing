37a5a4cf816096e1507f44f5c89c040a8f16296e
mattcraigie <matthewcraigie1@gmail.com>
2023-06-09 17:42:25 +1000

    removing notebook

--- scripts/subset_test_ost_ddp.py
+++ scripts/subset_test_ost_ddp.py
@@ -15,8 +15,8 @@ from ostlensing.models import OptimisableSTRegressor
 
 def setup(rank, world_size):
     # use these if using a notebook
-    os.environ['MASTER_ADDR'] = 'localhost'
-    os.environ['MASTER_PORT'] = '12355'
+    # os.environ['MASTER_ADDR'] = 'localhost'
+    # os.environ['MASTER_PORT'] = '12355'
 
     # initialize the process group
     dist.init_process_group("gloo", rank=rank, world_size=world_size)
