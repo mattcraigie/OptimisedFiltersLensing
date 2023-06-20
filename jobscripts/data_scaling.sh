d7086e1807a61ff81dba8968b859cd99a0aae84b
mcraigie <matthewcraigie1@gmail.com>
2023-06-20 14:34:40 +1000

    print debugging mp

--- jobscripts/data_scaling.sh
+++ jobscripts/data_scaling.sh
@@ -4,7 +4,7 @@
 #SBATCH -q regular
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node 1
-#SBATCH --cpus-per-task 1
+#SBATCH --cpus-per-task 16
 #SBATCH --gpus-per-node 4
 #SBATCH --time=1:00:00
 #SBATCH -J datascaling
