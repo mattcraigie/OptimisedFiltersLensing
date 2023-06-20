7a22c5bf38aa3332f0d632a4ff8f5f5e2e1a6d35
mcraigie <matthewcraigie1@gmail.com>
2023-06-20 11:56:46 +1000

    pre-augmenting

--- analysis/augment_patches.py
+++ analysis/augment_patches.py
@@ -18,11 +18,16 @@ def batch_apply(data, bs, func, operate_device, end_device=None):
 
 def main():
 
+    use_log = True
+
     op_dev = torch.device('cuda')
     end_dev = torch.device('cpu')
 
     load_path = '//pscratch/sd/m/mcraigie/cosmogrid/patches/'
-    save_path = '//pscratch/sd/m/mcraigie/cosmogrid/patches_log_std/'
+    if use_log:
+        save_path = '//pscratch/sd/m/mcraigie/cosmogrid/patches_log_std/'
+    else:
+        save_path = '//pscratch/sd/m/mcraigie/cosmogrid/patches_std/'
 
     all_dirs = os.listdir(os.path.join(load_path))
     all_dirs = np.sort(all_dirs)
@@ -33,17 +38,13 @@ def main():
         data.append(fields)
 
     data = torch.stack(data)
-    print(data.shape)
 
-    # first calculate log
-    data_log = batch_apply(data, 4, torch.log, operate_device=op_dev, end_device=end_dev)
+    if use_log:
+        data = batch_apply(data, 16, torch.log, operate_device=op_dev, end_device=end_dev)
 
-
-    # then calculate scaling values and apply
-    logged_mean = torch.mean(data_log)
-    logged_std = torch.std(data_log)
-    scaler = Scaler(logged_mean, logged_std)
-    result = batch_apply(data, 4, scaler.transform, operate_device=op_dev, end_device=end_dev)
+    #  calculate scaling values and apply
+    scaler = Scaler(torch.mean(data), torch.std(data))
+    result = batch_apply(data, 16, scaler.transform, operate_device=op_dev, end_device=end_dev)
 
     if not os.path.exists(save_path):
         os.makedirs(save_path)
