4a572ad23edc5c9799874aee2e847c0ce3322915
mattcraigie <matthewcraigie1@gmail.com>
2023-07-03 19:50:11 +1000

    fixing

--- analysis/pre_calculate.py
+++ analysis/pre_calculate.py
@@ -45,29 +45,38 @@ def pk(size, num_bins, device):
     return ps
 
 
-def pre_calc(load_path, save_path, save_name, method, kwargs):
+def pre_calc(load_path, save_path, save_name, method, kwargs, subset=None):
     function_mapping = {'ost': ost, 'mst': mst, 'ps': pk, 'resnet': resnet}
     raw_data = load_and_apply(load_path, function_mapping[method](**kwargs), device=torch.device('cuda:0'))
+
+    if subset is not None:
+        raw_data = raw_data[:subset]
+
     raw_data = raw_data.mean(axis=1)  # average over the patch axis
 
+    if subset is None:
+        subset = 'full'
+
+    main_path = os.path.join(save_path, f'{save_name}_{subset}')
+
     # without altering data
-    np.save(os.path.join(save_path, f'{save_name}.npy'), raw_data)
+    np.save(main_path + '.npy', raw_data)
 
     # with std
     scaler = Scaler(np.mean(raw_data), np.std(raw_data))
     std_data = scaler.transform(raw_data)
-    np.save(os.path.join(save_path, f'{save_name}_std.npy'), std_data)
+    np.save(main_path + '_std.npy', std_data)
 
     # with std and log
     log_data = np.log(raw_data)
     scaler = Scaler(np.mean(log_data), np.std(log_data))
     log_std_data = scaler.transform(log_data)
-    np.save(os.path.join(save_path, f'{save_name}_log_std.npy'), log_std_data)
+    np.save(main_path + '_log_std.npy', log_std_data)
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--config', default='configs/ps.yml', type=str)
+    parser.add_argument('-c', '--config', default='configs/precalc/ps.yml', type=str)
     args = parser.parse_args()
 
     with open(args.config, 'r') as file:
