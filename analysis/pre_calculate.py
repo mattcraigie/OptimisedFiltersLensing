f9942e15d7c1cead3ac0e1b5a58ab1ae7cf96d39
mcraigie <matthewcraigie1@gmail.com>
2023-07-04 15:55:27 +1000

    debugging

--- analysis/pre_calculate.py
+++ analysis/pre_calculate.py
@@ -34,7 +34,7 @@ def ost(filter_path, reduction, device):
 
 def resnet(model_state_dict_path, pretrained_model, device):
     model = ResNetWrapper(pretrained_model=pretrained_model)
-    model = model.load_state_dict(torch.load(model_state_dict_path))
+    model.load_state_dict(torch.load(model_state_dict_path))
     print(model)
     model.to(device)
     return model
