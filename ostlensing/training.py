261b398d599daa3b2021cf311b1da5c4ea4e9013
mcraigie <matthewcraigie1@gmail.com>
2023-07-12 08:19:07 +1000

    output shape debug

--- ostlensing/training.py
+++ ostlensing/training.py
@@ -145,7 +145,7 @@ class Trainer:
     def make_predictions(self):
         self.regressor.eval()
         print(self.device, self.val_loader.dataset.data.shape)
-        print(self.device, self.val_loader.dataset.data[:, 0, 0])
+        print(self.device, self.val_loader.dataset.data[:, 0, 0, 0])
         with torch.no_grad():
             self.train_pred, self.train_targets = dataloader_apply(self.train_loader, self.regressor, self.device)
             self.val_pred, self.val_targets = dataloader_apply(self.val_loader, self.regressor, self.device)
